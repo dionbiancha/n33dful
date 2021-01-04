6e790128cda96f64678a32abc44e2e82a3bcb09e
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-04 16:42:56 -0300

    Login structure part 2

--- src/helpers/AuthHandler.js
+++ src/helpers/AuthHandler.js
@@ -3,4 +3,13 @@ import Cookies from 'js-cookie';
 export const isLogged = () => {
   let token = Cookies.get('token');
   return (token) ? true : false;
+  if(rememberPassword) {
+    Cookies.set('token', token, {expires: 999});
+  } else {
+    Cookies.set('token', token);
+  }
+};
+
+export const doLoggin = (token, rememberPassword = false) => {
+
 };
---
+++ src/helpers/N33dfulAPI.js
@@ -0,0 +1,9 @@
+
+const N33dfulAPI = {
+  login: async(email, password) => {
+    //consulta ao BD
+    return {};
+  }
+};
+
+export default () => N33dfulAPI;
