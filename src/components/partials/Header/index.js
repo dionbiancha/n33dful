ab7031daf38d5e9dfa39430cf05713d343a701a1
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-04 11:22:58 -0300

    Create Header part 1

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import { HeaderArea } from './styled';
+
 
 const Header = () => {
   return (
     <HeaderArea>
-      ...
+      <div className="container">
+        <div className="logo">
+          <Link to="/">
+            <span className="text-logo">n33dful</span>
+          </Link>
+        </div>
+      </div>
     </HeaderArea>
   );
 };
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,6 +1,31 @@
 import styled from 'styled-components';
 
 export const HeaderArea = styled.div`
-  height: 100px;
-  background-color: #CCC;
+background-color: #FFF;
+height: 60px;
+border-bottom: 1px solid #CCC;
+
+.container {
+  max-width: 1000px;
+  margin: auto;
+  display: flex;
+}
+a {
+  text-decoration: none;
+}
+.logo {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  height: 60px;
+
+  .text-logo {
+    font-size: 27px;
+    font-weight: bold;
+    text-shadow: 0px 0px 1px rgba(255,0,0,1),
+    0px 0px 13px rgba(255,0,0,0.6),
+    0px 0px 23px rgba(255,0,0,0.6);
+    color: #FFF
+  }
+}
 `;
