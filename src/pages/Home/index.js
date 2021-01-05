78b4e115b8d2aa42dd8696eeb2b7fd879a71401b
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-05 07:38:25 -0300

    Home search box

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { PageArea, SearchArea } from './styled';
+import { PageContainer } from '../../components/MainComponents';
+
+import useApi from '../../helpers/N33dfulAPI';
 
 const Page = () => {
+  const api = useApi();
+
   return(
-    <div>Home page</div>
+    <>
+      <SearchArea>
+        <PageContainer>
+          <div className="searchBox">
+            <form method="GET" action="/ads">
+              <input type="text" name="q" placeholder="O que você procura?"/>
+              <select name="state">
+
+              </select>
+              <button>Pesquisar</button>
+            </form>
+          </div>
+          <div className="categoryList">
+
+          </div>
+        </PageContainer>
+      </SearchArea>
+      <PageContainer>
+        <PageArea>
+          ...
+        </PageArea>
+    </PageContainer>
+    </>
   );
 };
 
