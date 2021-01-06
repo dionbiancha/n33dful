c24556dfc6b3591f517d0348fb703e94212e0a03
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-06 10:30:49 -0300

    AdPage request

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useParams } from 'react-router-dom';
 
@@ -12,7 +12,27 @@ const Page = () => {
   const { id } = useParams();
 
   const [loading, setLoading] = useState(true);
-  const [adInfo, setAdInfo] = useState([]);
+  const [adInfo, setAdInfo] = useState({});
+
+  const formatDate = (date) => {
+    let cDate = new Date(date);
+
+    let months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    let cDay = cDate.getDate();
+    let cMoths = cDate.getMonth();
+    let cYear = cDate.getFullYear();
+
+    return `${cDay} de ${months[cMoths]} de ${cYear}`;
+  }
+
+  useEffect(() => {
+    const getAdInfo = async (id) => {
+      const json = await api.getAd(id, true);
+      setAdInfo(json);
+      setLoading(false);
+    }
+    getAdInfo(id);
+  }, []);
 
   return(
     <PageContainer>
@@ -25,9 +45,20 @@ const Page = () => {
             <div className="adInfor">
               <div className="adName">
                 {loading && <Fake height={20}/>}
+                {adInfo.title &&
+                  <h2>{adInfo.title}</h2>
+                }
+                {adInfo.dateCreated && 
+                <small>Criado em {formatDate(adInfo.dateCreated)}</small>
+                }
               </div>
               <div className="adDescription">
                 {loading && <Fake height={100}/>}
+                {adInfo.description}
+                <hr/>
+                {adInfo.views && 
+                  <small>Visualizações: {adInfo.views}</small>
+                }
               </div>
             </div>
           </div>
