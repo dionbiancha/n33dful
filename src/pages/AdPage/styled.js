b5175c8281430ef88c6e37f46f18c6ca13074ad6
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-08 08:49:46 -0300

    AdPage right info

--- src/pages/AdPage/styled.js
+++ src/pages/AdPage/styled.js
@@ -70,5 +70,35 @@ export const PageArea = styled.div`
   
   .rightSide {
     width: 250px;
+
+    .price span {
+      color: #0000FF;
+      display: block;
+      font-size: 27px;
+      font-weight: bold;
+    }
+
+    .contactSellerLink {
+      background-color: #0000FF;
+      color: #FFF;
+      height: 30px;
+      border-radius: 5px;
+      box-shadow: 0px 0px 4px #999;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-decoration: none;
+      margin-bottom: 20px;
+    }
+
+    .createdBy strong {
+      display: block;
+    }
+
+    .createdBy small {
+      display: block;
+      color: #999;
+      margin-top: 10px;
+    }
   }
 `;
