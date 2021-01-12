910fc065a33717f3cce3349969ed075ee8461209
Dionei Bianchati <dion.biancha@gmail.com>
2021-01-12 11:41:02 -0300

    ads structure

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import AdPage from './pages/AdPage';
 import AddAd from './pages/AddAd';
+import Ads from './pages/Ads';
 import NotFound from './pages/NotFound';
 
 export default () => {
@@ -31,6 +32,9 @@ export default () => {
       </RouteHandler>
       <RouteHandler private exact path="/post-an-ad" >
         <AddAd />
+      </RouteHandler>
+      <RouteHandler exact path="/ads" >
+        <Ads />
       </RouteHandler>
       <RouteHandler>
         <NotFound />
